affa5609d54d99d2bf5756c10b8fa304bc222c20
bshakir <burhanshakir96@gmail.com>
2017-09-09 15:46:50 +0530

    Built iPhone interface

---
+++ dev-profile/RoundedCornerImage.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedCornerImage.swift
+//  dev-profile
+//
+//  Created by Burhanuddin Shakir on 09/09/17.
+//  Copyright © 2017 Burhanuddin Shakir. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+    }
+
+}
